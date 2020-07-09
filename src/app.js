9abe7a67af4be19e2fe465c9779e33e58cbba351
Matthias Radig <radig@riege.com>
2020-07-09 14:53:35 +0200

    Redux hello world

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 import {html, render} from 'https://unpkg.com/lit-html?module';
+
+const hello = function(state = {name: "world"}, message) {
+    const { type, payload } = message
+    switch (type) {
+        case 'set': return { name: payload }
+        default: return state
+    }
+}
+
+const store = Redux.createStore(hello)
+
+store.subscribe(_ => console.log(store.getState()))
+store.dispatch({type: 'xxx', payload: 'muli'})
+store.dispatch({type: 'set', payload: 'muli'})
