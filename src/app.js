97030d33a0f3d80f9b6affa4a4d8fafe89847be3
Matthias Radig <radig@riege.com>
2020-07-09 11:17:31 +0200

    Understanding the DOM

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 console.log("Hello, airhacks")
+
+const button = document.querySelector('button')
+const output = document.querySelector('output')
+const input = document.querySelector('input')
+
+button.onclick = e => output.innerHTML = 'button clicked'
+console.dir(button)
+
+input.onkeyup = e => output.innerHTML = `<h2>${e.target.value}</h2>`
