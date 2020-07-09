661e568a9da3c30c1675971b980b66978824d780
Matthias Radig <radig@riege.com>
2020-07-09 14:14:31 +0200

    Shadow DOM

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,19 @@
 import {html, render} from 'https://unpkg.com/lit-html?module';
 
 export default class Post extends HTMLElement {
+
+    constructor() {
+        super()
+        this.root = this.attachShadow({mode: "open"})
+    }
+
     connectedCallback() {
-        this.innerHTML = `
+        const template = html`
+            <style>header{background-color: #faf}</style>
+            <header>${new Date}</header>
             So I was at the dentist the other day ...
         `
+        render(template, this.root)
     }
 }
 
