0ba7f1ef5785c25282e0f4329248a5962f6e70ff
Matthias Radig <radig@riege.com>
2020-07-12 18:25:35 +0200

    Bind action log limit to a number field

--- src/examples/actionlogView.js
+++ src/examples/actionlogView.js
@@ -1,11 +1,18 @@
 import store from '../store.js'
 import { ReactiveElement } from '../elements.js'
 import { html } from 'https://unpkg.com/lit-html?module'
+import { SET_LIMIT } from './actionlogModel.js'
+
+const setLimit = e => store.dispatch(SET_LIMIT, e.target.value)
 
 class ActionLogView extends ReactiveElement {
 
     render() {
         return html `
+            <label for="limit">Limit:</label>
+            <input type="number" .value="${this.state.actionLog.limit}"
+                                 @change="${setLimit}">
+
             <ol reversed start="${this.state.actionLog.count}">
                 ${this.state.actionLog.log.map(this.actionLogItem)}
             </ol>
