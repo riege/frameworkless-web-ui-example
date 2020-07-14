c127e42a2a3cc7a09a2f852c62eb4a435a321d5c
Matthias Radig <radig@riege.com>
2020-07-14 08:44:20 +0200

    Bind basic game actions

--- src/examples/game/cards.js
+++ src/examples/game/cards.js
@@ -1,7 +1,7 @@
 const cards = {
-    ATTACK: { name: 'Attack', damage: 6 },
-    BLOCK: { name: 'Block', block: 6 },
-    DEFEND: { name: 'Block', block: 4, damage: 3 },
+    ATTACK: { name: 'Attack', damage: 6, description: 'Attack the enemy for 6 damage' },
+    BLOCK: { name: 'Block', block: 6, description: 'Increase your block by 6' },
+    DEFEND: { name: 'Defend', block: 4, damage: 3, description: 'Increase your block by 4 and attack the enemy for 3 damage' },
 }
 for (let k in cards) {
     cards[k] = Object.freeze(cards[k])
--- src/examples/game/game_view.js
+++ src/examples/game/game_view.js
@@ -1,5 +1,8 @@
 import { ReactiveElement } from '../../base/elements.js'
 import { html } from '../../deps/lit-html.js'
+import store from '../../base/store.js'
+import gameModel from './game_model.js'
+import { START_GAME, PLAY_CARD, END_TURN, STATE_WELCOME, STATE_GAME, STATE_GAME_OVER } from './game_model.js'
 
 function characterStats(char) {
     const row = (name, val) => html `<tr><td>${name}:</td><td>${val}</td></tr>`
@@ -14,7 +17,9 @@ function characterStats(char) {
 
 function player(player) {
     return html `
-        <div class="player-model"></div>
+        <div class="character-model player-model">
+            <h1>Player</h1>
+        </div>
         <div class="player-stats">
             ${characterStats(player)}
         </div>
@@ -23,31 +28,70 @@ function player(player) {
 
 function enemy(enemy) {
     return html `
-        <div class="enemy-model"></div>
+        <div class="character-model enemy-model">
+            <h1>Enemy</h1>
+            <div class="enemy-intent">
+            The enemy plans to
+            ${enemyIntent('Attack', enemy.willAttack)}
+            ${enemyIntent('Block', enemy.willBlock)}
+            ${enemyIntent('do nothing', !(enemy.willBlock || enemy.willAttack))}
+            </div>
+        </div>
         <div class="enemy-stats">
             ${characterStats(enemy)}
         </div>
         `
 }
 
+function enemyIntent(action, value) {
+    if (!value) {
+        return null
+    }
+    return html `<p>${action} ${typeof(value) === 'number' ? value : ''}</p>`
+}
+
 function cards(hand) {
     return html `
         <div class="player-hand">
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
+            ${hand.map(card)}
         </div>
     `
+}
+
+function card(card, index) {
+    const playCard = () => store.dispatch(PLAY_CARD, index)
+    return html `
+        <div class="card" @click="${playCard}">
+            <h1>${card.name}</h1>
+            <p>
+                ${card.description}
+            </p>
+        </div>
+    `
+}
+
+function arena(game) {
+    return html `<div class="arena">${arenaContent(game)}</div>`
+}
+
+function arenaContent(game) {
+    switch (game.state) {
+        case STATE_WELCOME:
+            return html `<button @click="${() => store.dispatch(START_GAME)}">Start new game</button>`
+        case STATE_GAME:
+            return html `<button @click="${() => store.dispatch(END_TURN)}">End turn</button>`
+        case STATE_GAME_OVER:
+            return html `<p>Game over</p><button @click="${() => store.dispatch(START_GAME)}">Start new game</button>`
+    }
 }
 
 class GameView extends ReactiveElement {
     render() {
         const game = this.state.game
         return html `
+            ${arena(game)}
             ${player(game.player)}
-            ${cards(game.player.hand)}
+            ${cards(game.cards.hand)}
             ${enemy(game.enemy)}
         `
     }
