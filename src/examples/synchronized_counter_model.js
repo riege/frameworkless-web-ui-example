4283b2651be0499a539910421f3f5eeea008ebea
Matthias Radig <radig@riege.com>
2020-07-16 17:07:11 +0200

    Example for synchronized counter list

--- src/examples/synchronized_counter_model.js
+++ src/examples/synchronized_counter_model.js
@@ -5,7 +5,22 @@ export default class SynchronizedCounterModel {
         this._counterA = counterModel.initialState()
         this._counterB = counterModel.initialState()
         this._counterC = counterModel.initialState()
+        this.counterCounter = counterModel.initialState()
+        this._counters = []
         this.synchronized = false
+        this.synchronizedList = false
+    }
+
+    get counters() {
+        for (let i = this._counters.length; i < this.counterCounter.value; i++) {
+            this._counters[i] = counterModel.initialState()
+        }
+        this._counters.splice(this.counterCounter.value, this._counters.length)
+        if (this.synchronizedList) {
+            const firstCounter = this._counters[0]
+            return this._counters.map(_ => firstCounter)
+        }
+        return this._counters
     }
 
     get counterA() {
@@ -32,14 +47,18 @@ export default class SynchronizedCounterModel {
 
 }
 
-export function enableSync(model) {
-    model.synchronized = true
-}
-
-export function disableSync(model) {
-    model.synchronized = false
-}
-
 export function setSync(model, value) {
     model.synchronized = value
+}
+
+export function addCounter(model) {
+    model._counters.push(counterModel.initialState())
+}
+
+export function removeCounter(model, i) {
+    model._counters.splice(i, i+1)
+}
+
+export function setListSync(model, value) {
+    model.synchronizedList = value
 }
--- src/examples/synchronized_counter_view.js
+++ src/examples/synchronized_counter_view.js
@@ -1,18 +1,32 @@
 import { ReactiveElement2 } from '../base/elements.js'
 import {html} from '../deps/lit-html.js'
-import { enableSync, disableSync, setSync } from './synchronized_counter_model.js'
+import { setSync, setListSync } from './synchronized_counter_model.js'
 
 class SynchronizedCounterView extends ReactiveElement2 {
 
+    counters(counters) {
+    return counters.map((_c, i) => html`<h5>Counter ${i}</h5> <counter-view model="${this.subModel(`counters.${i}`)}"></counter-view>`)
+    }
+
     render() {
         return html`
+            <h3>Syncronized Individual Counters</h3>
             <counter-view model="${this.subModel('counterA')}"></counter-view>
             <counter-view model="${this.subModel('counterB')}"></counter-view>
             <counter-view model="${this.subModel('counterC')}"></counter-view>
             <input class="sync" type="checkbox"
-                .checked="${this.state.synchronize}"
+                .checked="${this.state.synchronized}"
                 @change="${this.dispatchChecked(setSync)}"
             >synchronize</input>
+
+            <h3>Syncronized Counter List</h3>
+            <h5>Number of counters</h5>
+            <counter-view model="${this.subModel('counterCounter')}"></counter-view>
+            <input class="sync" type="checkbox"
+                .checked="${this.state.synchronizedList}"
+                @change="${this.dispatchChecked(setListSync)}"
+            >synchronize</input>
+            ${this.counters(this.state.counters)}
         `
     }
 }
