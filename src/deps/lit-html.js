695d3a7062d86c6f67c5fe614c7d179f2a9c75ba
Matthias Radig <radig@riege.com>
2020-07-27 10:58:50 +0200

    Use jsdelivr and fixed versions for external dependencies

--- src/deps/lit-html.js
+++ src/deps/lit-html.js
@@ -1,2 +1,2 @@
-import { html, render } from 'https://unpkg.com/lit-html@1.2.1/lit-html.js?module'
+import { html, render } from 'https://cdn.jsdelivr.net/npm/lit-html@1.2.1/lit-html.js'
 export { html, render }
