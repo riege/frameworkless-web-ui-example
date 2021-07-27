b4720de46e0dd7025e3d323655227ec74b394b2d
Matthias Radig <radig@riege.com>
2021-07-27 12:01:30 +0200

    Fix dependencies

--- src/deps/lit-html.js
+++ src/deps/lit-html.js
@@ -1,2 +1,2 @@
-import { html, render } from 'https://cdn.jsdelivr.net/npm/lit-html@1.2.1/lit-html.js'
+import { html, render } from 'https://cdn.jsdelivr.net/npm/lit-html@1.4.1/lit-html.js'
 export { html, render }
